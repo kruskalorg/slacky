6a712543f4b4776a7aeaf61f90e649548ac00a48
danny <fletd01@yahoo.com>
2014-04-16 21:58:46 -0500

    adding basic index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+var slackytoken = process.env.SLACKY_TOKEN;
+
+var MY_SLACK_WEBHOOK_URL = 'https://newcoco.slack.com/services/hooks/incoming-webhook?token=' + slackytoken;
+var slack = require('slack-notify')(MY_SLACK_WEBHOOK_URL);
+
+
+var express = require('express');
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/stashmessage', function(req, res) {
+
+    slack.send({
+        channel: '#deep-thoughts',
+        icon_emoji: ':1up:',
+        text: req.body["stripped-text"],
+        username: 'Slacky',
+        "mrkdwn": true
+    });
+
+    res.send('thanks');
+});
+
+app.listen(3343);
